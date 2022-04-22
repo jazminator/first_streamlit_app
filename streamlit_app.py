e0ea05130daa5adaef0d42a31ecd81948a4d0d81
jazminator <104039361+jazminator@users.noreply.github.com>
2022-04-22 10:46:51 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ if streamlit.button('Get Fruit Load List'):
   streamlit.dataframe(my_data_rows)
   
 # don't run anything past here while we troubleshoot
-streamlit.stop()
+# streamlit.stop()
 
 #New Section to let the user add a fruit
 def insert_row_snowflake(new_fruit):
